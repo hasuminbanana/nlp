98bf0368b4fc66369352eacbfcff6b2d33021b48
hasuminbanana <38072052+hasuminbanana@users.noreply.github.com>
2019-05-30 12:29:04 +0900

    Add files via upload

---
+++ JP_EN.py
@@ -0,0 +1,20 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+
+from bs4 import BeautifulSoup
+import requests
+import webbrowser
+
+def openff():
+    # This def open firefox browser.
+    Jpsen = input("英語にしたい日本語を入力してください: ")
+    url = "https://script.google.com/macros/s/AKfycby_dU9RymFwgEsd6txZAhA68MQAnUQxXeRnbVia_V0U8fv-Gkoy/exec?text=" + Jpsen + "&source=ja&target=en"
+    browser = webbrowser.get('chrome')
+    browser.open(url)
+
+openff()
+
